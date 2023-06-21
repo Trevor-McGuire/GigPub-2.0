a1c3a05fc32178012e1344781ad26b6892e457b7
Chris Messa <blindsun95@gmail.com>
2023-06-20 22:22:44 -0500

    before merge

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -30,3 +30,46 @@
   document
   .querySelectorAll('.newreview-form')
   .forEach(item => item.addEventListener('submit', reviewsFormHandler))
+
+  const commentsButtons = document.querySelectorAll('.comments-button');
+
+commentsButtons.forEach(button => {
+  button.addEventListener('click', async (event) => {
+    const venueId = event.target.parentElement.querySelector('.venue').dataset.venueid;
+
+    fetch(`/api/reviews/${venueId}`)
+    .then((response) => {
+      if (response.ok){
+      return response.json();
+    } else {
+        throw new Error('failed to fetch reviews');
+      }
+      })
+      .then((reviews) => {
+        console.log(reviews);
+        
+        // Assuming you have an element with the class "reviews-container" to display the reviews
+        const reviewsContainer = document.querySelector('.reviews-container');
+        
+        // Clear the existing reviews
+        reviewsContainer.innerHTML = '';
+        
+        // Iterate through the reviews and create HTML elements to display each review
+        reviews.forEach(review => {
+          const reviewElement = document.createElement('div');
+          reviewElement.innerHTML = `
+            <span>${review.text}</span>
+          `;
+          reviewsContainer.appendChild(reviewElement);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+        const card = event.target.closest('.card'); 
+        console.log(card)
+        const commentsSection = card.querySelector('.comments-card')
+        
+        document.querySelector('.reviews-container').style.display = document.querySelector.style.display === 'none' ? 'block' : 'none';
+  });
+});
